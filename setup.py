4f7e98fd6812dcb4d93bcc59a690d6e745e23e67
Michael Hearne <mhearne@usgs.gov>
2016-01-12 08:49:24 -0700

    Removed lsprocess.py from list of scripts

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='lsprocess',
       author_email='mhearne@usgs.gov',
       url='',
       packages=['lsprocess'],
-      scripts = ['secsample','subsample','lsprocess.py'],
+      scripts = ['secsample','subsample'],
 )
